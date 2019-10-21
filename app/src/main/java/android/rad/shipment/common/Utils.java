2a4b08db66e72ef454855146a4e4c21e94d3a6f6
Marcsllite <marcsllite@gmail.com>
2019-10-21 01:35:54 -0400

    updated reference-land layouts and fixed app:lint errors

--- app/src/main/java/android/rad/shipment/common/Utils.java
+++ app/src/main/java/android/rad/shipment/common/Utils.java
@@ -2,6 +2,7 @@ package android.rad.shipment.common;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 /**
  * This class contains static util methods
@@ -17,7 +18,7 @@ public class Utils {
      * Convert Unix timestamp to a human readable representation of date
      */
     public static String convertToHumanReadableDate(String timestamp) {
-        SimpleDateFormat fmtOut = new SimpleDateFormat();
+        SimpleDateFormat fmtOut = new SimpleDateFormat("", Locale.US);
         return fmtOut.format(new Date(Long.valueOf(timestamp)));
     }
 
