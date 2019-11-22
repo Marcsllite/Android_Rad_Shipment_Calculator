35d9e0d64a389dc69d42de404e88ad41afe992ad
marcsllite <marcsllite@gmail.com>
2019-11-22 14:28:46 -0500

    updated room database [skip travis]

--- app/src/main/java/android/rad/shipment/calculator/database/ShipmentCalculatorLocalDB.java
+++ app/src/main/java/android/rad/shipment/calculator/database/ShipmentCalculatorLocalDB.java
@@ -1,5 +1,6 @@
 package android.rad.shipment.calculator.database;
 
+import android.content.Context;
 import android.rad.shipment.calculator.database.dao.A1Dao;
 import android.rad.shipment.calculator.database.dao.A2Dao;
 import android.rad.shipment.calculator.database.dao.DecayConstantDao;
@@ -18,7 +19,7 @@ import android.rad.shipment.calculator.database.tables.DecayConstant;
 import android.rad.shipment.calculator.database.tables.ExemptConcentration;
 import android.rad.shipment.calculator.database.tables.ExemptLimit;
 import android.rad.shipment.calculator.database.tables.HalfLife;
-import android.rad.shipment.calculator.database.tables.IALimiteLimit;
+import android.rad.shipment.calculator.database.tables.IALimitedLimit;
 import android.rad.shipment.calculator.database.tables.IAPackageLimit;
 import android.rad.shipment.calculator.database.tables.Isotopes;
 import android.rad.shipment.calculator.database.tables.LicensingLimit;
@@ -26,23 +27,39 @@ import android.rad.shipment.calculator.database.tables.LimitedLimit;
 import android.rad.shipment.calculator.database.tables.ReportableQuantity;
 
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {Isotopes.class, A1.class, A2.class, DecayConstant.class,
-        ExemptConcentration.class, ExemptLimit.class, HalfLife.class, IALimiteLimit.class,
+        ExemptConcentration.class, ExemptLimit.class, HalfLife.class, IALimitedLimit.class,
         IAPackageLimit.class, LicensingLimit.class, LimitedLimit.class, ReportableQuantity.class},
         version = 1)
-public abstract class ShipmentCalculatorLocalDatabase extends RoomDatabase {
-    public IsotopesDao isotopesDao;
-    public A1Dao a1Dao;
-    public A2Dao a2Dao;
-    public DecayConstantDao decayConstantDao;
-    public ExemptConcentrationDao exemptConcentrationDao;
-    public ExemptLimitDao exemptLimitDao;
-    public HalfLifeDao halfLifeDao;
-    public IALimitedLimitDao iaLimitedLimitDao;
-    public IAPackageLimitDao iaPackageLimitDao;
-    public LicensingLimitDao licensingLimitDao;
-    public LimitedLimitDao limitedLimitDao;
-    public ReportableQuantityDao reportableQuantityDao;
+public abstract class ShipmentCalculatorLocalDB extends RoomDatabase {
+    private static volatile ShipmentCalculatorLocalDB INSTANCE;
+
+    public abstract IsotopesDao isotopesDao();
+    public abstract A1Dao a1Dao();
+    public abstract A2Dao a2Dao();
+    public abstract DecayConstantDao decayConstantDao();
+    public abstract ExemptConcentrationDao exemptConcentrationDao();
+    public abstract ExemptLimitDao exemptLimitDao();
+    public abstract HalfLifeDao halfLifeDao();
+    public abstract IALimitedLimitDao iaLimitedLimitDao();
+    public abstract IAPackageLimitDao iaPackageLimitDao();
+    public abstract LicensingLimitDao licensingLimitDao();
+    public abstract LimitedLimitDao limitedLimitDao();
+    public abstract ReportableQuantityDao reportableQuantityDao();
+    
+    public static ShipmentCalculatorLocalDB getInstance(Context context){
+        if(INSTANCE == null) {
+            synchronized (ShipmentCalculatorLocalDB.class) {
+                if(INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            ShipmentCalculatorLocalDB.class, "ShipmentCalculatorLocal.db")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
