d5a5c60e7edd441491ffc6e26252e88adfca0b4d
Marc PIerre <marcsllite@gmail.com>
2019-12-07 22:11:16 -0500

    created detailed isotope adapter and results layouts [skip travis]

--- app/src/main/java/android/rad/shipment/calculator/model/Isotope.java
+++ app/src/main/java/android/rad/shipment/calculator/model/Isotope.java
@@ -4,20 +4,28 @@ import android.rad.shipment.calculator.R;
 
 public class Isotope {
     // Declaring Variables
-    private int defaultVal = R.integer.defaultInt;
+    transient private int defaultVal = R.integer.defaultInt;
     private String _Name;                   // The name of the isotope
     private String _DBName;                 // The database search name of the isotope (including short/long or lung absorption)
     private float _A0;                      // Initial Activity (microCuries) of isotope
+    private float _AToday;                  // Activity of the isotope today (microCuries)
     private float _Mass;                    // the mass of the isotope (grams for solids, liters for liquids)
     private String _MassUnit;               // the mass unit for the isotope (grams or liter)
     private String _Nature;                 // the nature (regular, special) of the isotope
     private String _State;                  // the state (solid, liquid, gas) of the isotope
     private String _Form;                   // the form (normal, special) of the isotope
     private String _ShortLong;              // for isotopes with different half lives
-    private String _LungAbs;                  // the lung absorption speed if the isotope is Uranium (f = fast, m = medium, s = slow)
+    private String _LungAbs;                // the lung absorption speed if the isotope is Uranium (f = fast, m = medium, s = slow)
+    private float _HalfLife;                //
+    private float _DecayConst;              //
+    private float _DPM;                     //
+    private boolean _RQ;                    //
+    private float _LicLimPer;               //
+    private float _ActivityPer;             //
 
     private int _IsotopeClass;              // Classification of isotope as an integer
                                             // (0 = Exempt, 1 = Excepted, 2 = Type A, 4 = Type B, 8 = Type B: Highway Route Control)
+
     /*/////////////////////////////////////////////////// ISOTOPE ////////////////////////////////////////////////////*/
     /**
      * Constructs an Isotope object with the given name and initial activity
@@ -42,6 +50,13 @@ public class Isotope {
 
         _LungAbs = "";
         _ShortLong = "";
+        _AToday = defaultVal;
+        _HalfLife = defaultVal;
+        _DecayConst = defaultVal;
+        _DPM = defaultVal;
+        _RQ = false;
+        _LicLimPer = defaultVal;
+        _ActivityPer = defaultVal;
         _IsotopeClass = defaultVal;
     }
 
@@ -51,14 +66,24 @@ public class Isotope {
      * @param newIso the isotope to copy all the information from
      */
     public Isotope(Isotope newIso) {
-        _Name = newIso._Name;                 
+        _Name = newIso._Name;
+        _DBName = newIso._DBName;
         _A0 = newIso._A0;
         _Mass = newIso._Mass;
+        _MassUnit = newIso._MassUnit;
         _Nature = newIso._Nature;
         _State = newIso._State;
         _Form = newIso._Form;
-        _ShortLong = newIso._ShortLong;
+
         _LungAbs = newIso._LungAbs;
+        _ShortLong = newIso._ShortLong;
+        _AToday = newIso._AToday;
+        _HalfLife = newIso._HalfLife;
+        _DecayConst = newIso._DecayConst;
+        _DPM = newIso._DPM;
+        _RQ = newIso._RQ;
+        _LicLimPer = newIso._LicLimPer;
+        _ActivityPer = newIso._ActivityPer;
         _IsotopeClass = newIso._IsotopeClass;
     } 
     
@@ -67,6 +92,8 @@ public class Isotope {
         int result = 1;
 
         result = prime * result + ((_Name == null) ? 0 : _Name.hashCode()) +
+                                ((_DBName == null) ? 0 : _DBName.hashCode()) +
+                                ((_MassUnit == null) ? 0 : _MassUnit.hashCode()) +
                                 ((_Nature == null) ? 0 : _Nature.hashCode()) +
                                 ((_State == null) ? 0 : _State.hashCode()) +
                                 ((_Form == null) ? 0 : _Form.hashCode()) +
@@ -82,8 +109,10 @@ public class Isotope {
         Isotope other = (Isotope) obj;
 
         return other._Name.equals(this._Name) &&
+                other._DBName.equals(this._DBName) &&
                 other._A0 == this._A0 &&
                 other._Mass == this._Mass &&
+                other._MassUnit == this._MassUnit &&
                 other._Nature.equals(this._Nature) &&
                 other._State.equals(this._State) &&
                 other._Form.equals(this._Form);
@@ -110,6 +139,13 @@ public class Isotope {
      * @param A0 the new initial activity of the isotope in microCi
      */
     public void set_A0(float A0) { _A0 = A0; }
+
+    /**
+     * Setter function to set this isotope's current activity in microCi
+     *
+     * @param AToday the new current activity of the isotope in microCi
+     */
+    public void set_AToday(float AToday) { _AToday = AToday; }
 
     /**
      * Setter function to set this isotope's mass in grams for solids and liters for liquids
@@ -160,6 +196,18 @@ public class Isotope {
      */
     public void set_LungAbs(String lungAbs) { _LungAbs = lungAbs; }
 
+    public void set_HalfLife(float halflife) { _HalfLife = halflife; }
+
+    public void set_DecayConst(float decayConst) { _DecayConst = decayConst; }
+
+    public void set_DPM(float dpm) { _DPM = dpm; }
+
+    public void set_RQ(boolean rq) { _RQ = rq; }
+
+    public void set_LicLimPer(float licLimPer) { _LicLimPer = licLimPer; }
+
+    public void set_ActivityPer(float activityPer) { _ActivityPer = activityPer; }
+    
     /**
      * Setter function to set this isotope's classification as an integer
      *  0 = Exempt Classification
@@ -194,6 +242,13 @@ public class Isotope {
      * @return the initial activity of this isotope in microCi
      */
     public float get_A0() { return _A0; }
+
+    /**
+     * Getter function to get this isotope's current activity in microCi
+     *
+     * @return the current activity of this isotope in microCi
+     */
+    public float get_AToday() { return _AToday; }
 
     /**
      * Getter function to get this isotope's mass in grams for solids and liters for liquids
@@ -244,6 +299,18 @@ public class Isotope {
      */
     public String get_LungAbs() { return _LungAbs; }
 
+    public float get_HalfLife() { return _HalfLife; }
+
+    public float get_DecayConst() { return _DecayConst; }
+
+    public float get_DPM() { return _DPM; }
+
+    public boolean get_RQ() { return _RQ; }
+
+    public float get_LicLimPer() { return _LicLimPer; }
+
+    public float get_ActivityPer() { return _ActivityPer; }
+    
     /**
      * Getter function to get this isotope's classification as an integer
      *  0 = Exempt Classification
@@ -255,4 +322,25 @@ public class Isotope {
      * @return the classification of this isotope as an integer
      */
     public int get_IsotopeClass() { return _IsotopeClass; }
+
+    /**
+     * Getter function to get this isotope's classification as a string
+     *  0 = Exempt Classification
+     *  1 = Excepted/Limited Classification
+     *  2 = Type A Classification
+     *  4 = Type B Classification
+     *  8 = Type B: Highway Route Control Classification
+     *
+     * @return the classification of this isotope as a string
+     */
+    public String get_StringIsotopeClass() {
+        switch (_IsotopeClass){
+            case 0: return "Exempt";
+            case 1: return "Excepted/Limited";
+            case 2: return "Type A";
+            case 4: return "Type B";
+            case 8: return "Type B: Highway Route Control";
+            default: return "Invalid Classification";
+        }
+    }
 }
---
+++ app/src/main/java/android/rad/shipment/calculator/utils/DetailedIsotopeAdapter.java
@@ -0,0 +1,83 @@
+package android.rad.shipment.calculator.utils;
+
+import android.content.Context;
+import android.rad.shipment.calculator.R;
+import android.rad.shipment.calculator.model.Isotope;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * @author
+ *
+ * Adapter to take an Isotope and map it's data to a View to be shown in
+ * the listView
+ */
+public class DetailedIsotopeAdapter extends ArrayAdapter<Isotope> {
+    // View lookup cache
+    private static class ViewHolder {
+        TextView txtViewName;
+        TextView txtViewA0;
+        TextView txtViewAToday;
+        TextView txtViewHalfLife;
+        TextView txtViewDecayConst;
+        TextView txtViewDPM;
+        TextView txtViewClass;
+        TextView txtViewRQ;
+        TextView txtViewLicLimPer;
+        TextView txtViewActivityPer;
+    }
+
+
+    public DetailedIsotopeAdapter(Context context, ArrayList<Isotope> isotopes) {
+        super(context, R.layout.isotope_item, isotopes);
+    }
+
+    @Override public View getView(int position, View convertView, ViewGroup parent) {
+        Isotope isotope = getItem(position);  // Get the data item for this position
+        ViewHolder viewHolder; // view lookup cache stored in tag
+
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            // If there's no view to re-use, inflate a brand new view for row
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.isotope_item, parent, false);
+
+            viewHolder.txtViewName = convertView.findViewById(R.id.txtViewName);
+            viewHolder.txtViewA0 = convertView.findViewById(R.id.txtViewA0);
+            viewHolder.txtViewAToday = convertView.findViewById(R.id.txtViewAToday);
+            viewHolder.txtViewHalfLife = convertView.findViewById(R.id.txtViewHalfLife);
+            viewHolder.txtViewDecayConst = convertView.findViewById(R.id.txtViewDecayConst);
+            viewHolder.txtViewDPM = convertView.findViewById(R.id.txtViewDPM);
+            viewHolder.txtViewClass = convertView.findViewById(R.id.txtViewClass);
+            viewHolder.txtViewRQ = convertView.findViewById(R.id.txtViewRQ);
+            viewHolder.txtViewLicLimPer = convertView.findViewById(R.id.txtViewLicLimPer);
+            viewHolder.txtViewActivityPer = convertView.findViewById(R.id.txtViewActivityPer);
+            // Cache the viewHolder object inside the fresh view
+            convertView.setTag(viewHolder);
+        } else {
+            // View is being recycled, retrieve the viewHolder object from tag
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        // Populate the data from the data object via the viewHolder object
+        // into the template view.
+        viewHolder.txtViewName.setText(isotope.get_Name().toUpperCase());
+        viewHolder.txtViewA0.setText(Float.toString(isotope.get_A0()));
+        viewHolder.txtViewAToday.setText(Float.toString(isotope.get_AToday()));
+        viewHolder.txtViewHalfLife.setText(Float.toString(isotope.get_HalfLife()));
+        viewHolder.txtViewDecayConst.setText(Float.toString(isotope.get_DecayConst()));
+        viewHolder.txtViewDPM.setText(Float.toString(isotope.get_DPM()));
+        viewHolder.txtViewClass.setText(isotope.get_StringIsotopeClass());
+        viewHolder.txtViewRQ.setText(Boolean.toString(isotope.get_RQ()));
+        viewHolder.txtViewLicLimPer.setText(Float.toString(isotope.get_LicLimPer()));
+        viewHolder.txtViewActivityPer.setText(Float.toString(isotope.get_ActivityPer()));
+        // Return the completed view to render on screen
+        return convertView;
+    }
+}
