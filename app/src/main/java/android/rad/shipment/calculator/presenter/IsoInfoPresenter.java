2d54665ec78debcabb6241bf54ce7f3413ee0f33
Marcsllite <marcsllite@gmail.com>
2019-12-08 20:41:27 -0500

    updating iso_info layout, view, presenter [skip travis]

--- app/src/main/java/android/rad/shipment/calculator/presenter/IsoInfoPresenter.java
+++ app/src/main/java/android/rad/shipment/calculator/presenter/IsoInfoPresenter.java
@@ -7,8 +7,6 @@ import android.rad.shipment.calculator.task.AppTask;
 import android.rad.shipment.calculator.task.TaskExecutor;
 import android.rad.shipment.calculator.view.IsoInfoDialogueView;
 
-import java.util.List;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
@@ -30,7 +28,7 @@ public class IsoInfoPresenter extends BasePresenter {
         mShipmentCalculatorDB = db;
     }
 
-    public void initViews(String abbr) { }
+    public void initViews(String abbr) { mTaskExecutor.async(new FetchIsotopeInfoTask(abbr)); }
 
     /*//////////////////////////////////////// LISTENERS /////////////////////////////////////////*/
 
@@ -41,11 +39,34 @@ public class IsoInfoPresenter extends BasePresenter {
 
     /*////////////////////////////////////////// TASKS ///////////////////////////////////////////*/
 
-    private class FetchIsotopeInfoTask implements AppTask<List<ShortLong>> {
+    private class FetchIsotopeInfoTask implements AppTask<Void> {
         private final String mAbbr;
         private boolean isShortLong;
-        private String _isoName, _A1Short, _A1Long, _A2Short, _A2Long, _DecayConst, _ExemptCon, _ExemptLim, _HalfLifeShort,
-                        _HalfLifeLong, _LicLim, _RQ;
+        private String _isoName, 
+                _A1, 
+                _A2,
+                _DecayConst, 
+                _ExemptCon, 
+                _ExemptLim, 
+                _HalfLife,
+                _LicLim, 
+                _RQ,
+                _A1Short,
+                _A2Short,
+                _DecayConstShort,
+                _ExemptConShort,
+                _ExemptLimShort,
+                _HalfLifeShort,
+                _LicLimShort,
+                _RQShort,
+                _A1Long,
+                _A2Long,
+                _DecayConstLong,
+                _ExemptConLong,
+                _ExemptLimLong,
+                _HalfLifeLong,
+                _LicLimLong,
+                _RQLong;
 
         public FetchIsotopeInfoTask(String abbr) {
             mAbbr = abbr;
@@ -53,53 +74,78 @@ public class IsoInfoPresenter extends BasePresenter {
         }
 
         @Override
-        public List<ShortLong> execute() {
+        public Void execute() {
+            _isoName = mShipmentCalculatorDB.getName(mAbbr) + " (" + mAbbr + ")";
+            
             for(ShortLong sl: mShipmentCalculatorDB.getAllShortLong()) {
                 if(sl.getAbbr().equalsIgnoreCase(mAbbr)) isShortLong = true;
             }
 
             if(isShortLong) {
                 _A1Short = Float.toString(mShipmentCalculatorDB.getA1(mAbbr + "(short)"));
-                _A1Long = Float.toString(mShipmentCalculatorDB.getA1(mAbbr + "(long)"));
                 _A2Short = Float.toString(mShipmentCalculatorDB.getA2(mAbbr + "(short)"));
-                _A2Long = Float.toString(mShipmentCalculatorDB.getA2(mAbbr + "(long)"));
+                _DecayConstShort = Float.toString(mShipmentCalculatorDB.getDecayConstant(mAbbr + "(short)"));
+                _ExemptConShort = Float.toString(mShipmentCalculatorDB.getExemptConcentration(mAbbr + "(short)"));
+                _ExemptLimShort = Float.toString(mShipmentCalculatorDB.getExemptLimit(mAbbr + "(short)"));
                 _HalfLifeShort = Float.toString(mShipmentCalculatorDB.getHalfLife(mAbbr + "(short)"));
-                _HalfLifeLong = Float.toString(mShipmentCalculatorDB.getHalfLife(mAbbr + "(long)"));
-            } else {
-                _A1Short = Float.toString(mShipmentCalculatorDB.getA1(mAbbr));
-                _A2Short = Float.toString(mShipmentCalculatorDB.getA2(mAbbr));
-                _HalfLifeShort = Float.toString(mShipmentCalculatorDB.getHalfLife(mAbbr));
-                _A1Long = null;
-                _A2Long = null;
-                _HalfLifeLong = null;
-            }
+                _LicLimShort = Float.toString(mShipmentCalculatorDB.getLicensingLimit(mAbbr + "(short)"));
+                _RQShort = Float.toString(mShipmentCalculatorDB.getReportableQuantity(mAbbr + "(short)"));
 
-            _isoName = mShipmentCalculatorDB.getName(mAbbr) + " (" + mAbbr + ")";
-            _DecayConst = Float.toString(mShipmentCalculatorDB.getDecayConstant(mAbbr));
-            _ExemptCon = Float.toString(mShipmentCalculatorDB.getExemptConcentration(mAbbr));
-            _ExemptLim = Float.toString();
-            _LicLim = ;
-            _RQ = ;
-            return mShipmentCalculatorDB.getAllShortLong();
+                _A1Long = Float.toString(mShipmentCalculatorDB.getA1(mAbbr + "(long)"));
+                _A2Long = Float.toString(mShipmentCalculatorDB.getA2(mAbbr + "(long)"));
+                _DecayConstLong = Float.toString(mShipmentCalculatorDB.getDecayConstant(mAbbr + "(long)"));
+                _ExemptConLong = Float.toString(mShipmentCalculatorDB.getExemptConcentration(mAbbr + "(long)"));
+                _ExemptLimLong = Float.toString(mShipmentCalculatorDB.getExemptLimit(mAbbr + "(long)"));
+                _HalfLifeLong = Float.toString(mShipmentCalculatorDB.getHalfLife(mAbbr + "(long)"));
+                _LicLimLong = Float.toString(mShipmentCalculatorDB.getLicensingLimit(mAbbr + "(long)"));
+                _RQLong = Float.toString(mShipmentCalculatorDB.getReportableQuantity(mAbbr + "(long)"));
+            } else {
+                _A1 = Float.toString(mShipmentCalculatorDB.getA1(mAbbr));
+                _A2 = Float.toString(mShipmentCalculatorDB.getA2(mAbbr));
+                _DecayConst = Float.toString(mShipmentCalculatorDB.getDecayConstant(mAbbr));
+                _ExemptCon = Float.toString(mShipmentCalculatorDB.getExemptConcentration(mAbbr));
+                _ExemptLim = Float.toString(mShipmentCalculatorDB.getExemptLimit(mAbbr));
+                _HalfLife = Float.toString(mShipmentCalculatorDB.getHalfLife(mAbbr));
+                _LicLim = Float.toString(mShipmentCalculatorDB.getLicensingLimit(mAbbr));
+                _RQ = Float.toString(mShipmentCalculatorDB.getReportableQuantity(mAbbr));
+            }
+            
+            return null;
         }
 
         @Override
-        public void onPostExecute(@Nullable List<ShortLong> result) {
+        public void onPostExecute(@Nullable Void result) {
             // setting values
-            mView.setTxtViewName(_isoName);
-            mView.setTxtViewA1(_A1);
-            mView.setTxtViewA2(_A2);
-            mView.setTxtViewDecayConst(_DecayConst);
-            mView.setTxtViewExemptCon(_ExemptCon);
-            mView.setTxtViewExemptLim(_ExemptLim);
-            mView.setTxtViewHalfLifeShort(_HalfLifeShort);
-            mView.setTxtViewLicLim(_LicLim);
-            mView.setTxtViewRQ(_RQ);
-
-            if(isShortLong) mView.disableShortLong();
-            else {
+            if(isShortLong) {
                 mView.enableShortLong();
+                
+                mView.setTxtViewA1Short(_A1Short);
+                mView.setTxtViewA2Short(_A2Short);
+                mView.setTxtViewDecayConstShort(_DecayConstShort);
+                mView.setTxtViewExemptConShort(_ExemptConShort);
+                mView.setTxtViewExemptLimShort(_ExemptLimShort);
+                mView.setTxtViewHalfLifeShort(_HalfLifeShort);
+                mView.setTxtViewLicLimShort(_LicLimShort);
+                mView.setTxtViewRQShort(_RQShort);
+
+                mView.setTxtViewA1Long(_A1Long);
+                mView.setTxtViewA2Long(_A2Long);
+                mView.setTxtViewDecayConstLong(_DecayConstLong);
+                mView.setTxtViewExemptConLong(_ExemptConLong);
+                mView.setTxtViewExemptLimLong(_ExemptLimLong);
                 mView.setTxtViewHalfLifeLong(_HalfLifeLong);
+                mView.setTxtViewLicLimLong(_LicLimLong);
+                mView.setTxtViewRQLong(_RQLong);
+            } else {
+                mView.disableShortLong();
+                mView.setTxtViewA1(_A1);
+                mView.setTxtViewA2(_A2);
+                mView.setTxtViewDecayConst(_DecayConst);
+                mView.setTxtViewExemptCon(_ExemptCon);
+                mView.setTxtViewExemptLim(_ExemptLim);
+                mView.setTxtViewHalfLife(_HalfLife);
+                mView.setTxtViewLicLim(_LicLim);
+                mView.setTxtViewRQ(_RQ);
             }
         }
     }
